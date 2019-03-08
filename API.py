092e98ea5483674a82bb0968994b1d156fefa50c
James Youngchae Chee <litcoderr@gmail.com>
2019-03-09 01:16:19 +0900

    edited sharable id

--- API.py
+++ API.py
@@ -10,12 +10,12 @@ from collections import OrderedDict
 class Model:
     def __init__(self, n_gpu):
         #TODO set pretrained model path
-        self.root_path = self.get_path()  # pre-trained weight path
+        self.path = self.get_path()  # pre-trained weight path
         self.n_gpu = n_gpu  # number of GPUs
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.pretrained_url = "https://drive.google.com/file/d/1ZzvYv_KRmf6arhqYUomRgAt3iZBtNyZu/view"
+        self.pretrained_url_id = "1ZzvYv_KRmf6arhqYUomRgAt3iZBtNyZu"
         self.downloader = Downloader()
 
     def getModel(self):
@@ -25,14 +25,14 @@ class Model:
         model = model.eval()
 
         # ******** Download from google drive ********
-        self.downloader.download(self.pretrained_url, self.root_path)
+        self.downloader.download(self.pretrained_url_id, self.path)
 
         # 1-1. check if need data parallel
         if self.n_gpu >= 2:
             model = nn.DataParallel(model)
 
         # 1-2. Load pre-trained model
-        state_dict = torch.load(os.path.join(self.root_path, "model"), map_location=self.device)
+        state_dict = torch.load(self.path, map_location=self.device)
 
         # 1-3. check if need to manipulate
         if self.n_gpu < 2:
@@ -49,7 +49,7 @@ class Model:
 
     def get_path(self):
         root = os.path.dirname(os.path.realpath(__file__))
-        path = os.path.join(root,"pretrained")
+        path = os.path.join(root,"pretrained","model")
         return path
 
 
